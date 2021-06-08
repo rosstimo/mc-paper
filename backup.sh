ea4e59e0c0fd48b299c96dd998c6ece02f6a726d
rosstimo <rosstimo@isu.edu>
2021-06-08 07:23:51 -0600

    fix "add directory" test

--- backup.sh
+++ backup.sh
@@ -1,11 +1,20 @@
-#!/bin/bash"
+#!/bin/bash
 TIME_STAMP="$(date '+%Y-%m-%d_%H:%M:%S')"
-PRODUCTION_PATH="/minecraft/production"
+SOURCE_PATH="/minecraft"
 BACKUP_DIR="/minecraft/backups"
 BACKUP_PATH="${BACKUP_DIR}/${TIME_STAMP}"
+
+if [ -d "${BACKUP_DIR}" ]; then
+    #pass
+else
+    mkdir "${BACKUP_DIR}"
+    wait $!
+fi
+
 mkdir "${BACKUP_PATH}"
+
 rsync -varn \
     --log-file="${BACKUP_DIR}/backup.log" \
-     "{PRODUCTION_PATH}" \
-    #--exclude="" \
+     "{SOURCE_PATH}" \
+    --exclude="/backups/*" \
     "${BACKUP_PATH}"
