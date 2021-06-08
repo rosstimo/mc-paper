92f2e9a7b6d89ebd39a622f52872d0f385644b82
rosstimo <rosstimo@isu.edu>
2021-06-08 11:03:29 -0600

    fix mkdir test

--- backup.sh
+++ backup.sh
@@ -4,17 +4,15 @@ SOURCE_PATH="/minecraft"
 BACKUP_DIR="/minecraft/backups"
 BACKUP_PATH="${BACKUP_DIR}/${TIME_STAMP}"
 
-if [ -d "${BACKUP_DIR}" ]; then
-    #pass
-else
+if [ ! -d "${BACKUP_DIR}" ]; then
     mkdir "${BACKUP_DIR}"
     wait $!
 fi
 
 mkdir "${BACKUP_PATH}"
 
-rsync -varn \
+rsync -varc \
     --log-file="${BACKUP_DIR}/backup.log" \
-     "{SOURCE_PATH}" \
+     "${SOURCE_PATH}" \
     --exclude="/backups/*" \
     "${BACKUP_PATH}"
